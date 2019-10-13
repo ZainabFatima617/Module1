18e59cdf30c0d4378d002f33c694e68baf458b06
ZainabFatima617 <55453560+ZainabFatima617@users.noreply.github.com>
2019-10-14 00:27:53 +0500

    IsPangram - VB
    Checks for whether the string is a Pangram or not.

---
+++ IsPangram.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String
+        Dim myAlpha As String
+        Dim isPangram As Boolean
+        Dim myChar As String
+
+        Str1 = ""
+        myAlpha = ""
+        isPangram = True
+        myChar = ""
+
+        Console.Write("Enter String to check whether it is a pangram or not: ")
+        Str1 = Console.ReadLine
+
+        isPangram = True
+        Str1 = UCase(Str1)
+        For ASCII = 65 To 90
+            myAlpha = Chr(ASCII)
+            myChar = InStr(Str1, myAlpha)
+            If myChar >= 1 Then
+                isPangram = True
+            Else
+                isPangram = False
+                Exit For
+            End If
+        Next
+
+        If isPangram = True Then
+            Console.WriteLine("The string is pangram.")
+            Console.ReadLine()
+        Else : Console.WriteLine("The string is not pangram.")
+        End If
+
+        Console.ReadKey()
+    End Sub
+
+End Module
