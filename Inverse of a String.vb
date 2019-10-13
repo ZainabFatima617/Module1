09c877f64f8fed9de668ad40b8dbd0e54590e572
ZainabFatima617 <55453560+ZainabFatima617@users.noreply.github.com>
2019-10-14 00:15:58 +0500

    Inverse of a String (Code 05

---
+++ Inverse of a String.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1 As String = ""
+        Dim myChar As String = ""
+        Dim Counter As Integer = 0
+        Dim FinalStr As String = ""
+
+        Console.Write("Enter String to inverse: ")
+        Str1 = Console.ReadLine
+
+        For Counter = Len(Str1) To 1 Step -1
+            myChar = Mid(Str1, Counter, 1)
+            FinalStr = FinalStr & myChar
+        Next
+
+        Console.WriteLine("Final String is: " & FinalStr)
+        Console.ReadKey()
+    End Sub
+
+End Module
