98712422b0f83dfc43ae4146308ed852a14b941a
ZainabFatima617 <55453560+ZainabFatima617@users.noreply.github.com>
2019-10-14 00:30:56 +0500

    Picking the middle word (Code 02) - VB
    Result shows the middle word of a string that consists of total three words.

---
+++ Picks up the middle word.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim A1 As String
+        Dim X As Integer
+        Dim A2 As String
+        Dim Y As Integer
+
+        A1 = ""
+        X = 0
+        A2 = ""
+        Y = 0
+
+        Console.Write("Enter String: ")
+        A1 = Console.ReadLine
+
+        X = InStr(A1, " ")
+        Y = InStr(X + 1, A1, " ")
+        A2 = Mid(A1, (X + 1), (Y - X - 1))
+
+        Console.WriteLine("Middle word of the string: " & A2)
+        Console.ReadKey()
+    End Sub
+
+End Module
