ed95aaea9b20a287e4212217d59dbb3deb7662cc
ZainabFatima617 <55453560+ZainabFatima617@users.noreply.github.com>
2019-10-14 00:32:27 +0500

    Removing a Character (Code 04) - VB
    The result shows a string after truncating specified character.

---
+++ Removing a Character.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myStr As String = ""
+        Dim Counter As Integer = 0
+        Dim NextChar As String = ""
+        Dim Character As String = ""
+        Dim FinalStr As String = ""
+
+        Console.Write("Enter String to remove characters: ")
+        myStr = Console.ReadLine
+
+        Console.Write("Enter Character to remove: ")
+        Character = Console.ReadLine
+
+        For Counter = 1 To Len(myStr)
+            NextChar = Mid(myStr, Counter, 1)
+            If NextChar <> Character Then
+                FinalStr = FinalStr & NextChar
+            End If
+        Next
+
+        Console.WriteLine("Final String is: " & FinalStr)
+        Console.ReadKey()
+    End Sub
+
+End Module
